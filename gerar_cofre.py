4b3ec0d1d43c69e2417181be4fc846b2ab3dc091
whynotsanty <goncalosantiago12@gmail.com>
2026-04-25 10:11:35 +0100

    imagem atualizada da analise de rtg limite

--- gerar_cofre.py
+++ gerar_cofre.py
@@ -7,7 +7,7 @@ from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 password = input("Define a password mestra para trancar as chaves: ").encode()
 
 # Derivar uma chave forte a partir da password
-salt = b'GSR_UM_2026' # Salt fixo para simplificar o projeto
+salt = b'GSR_UM_2026'
 kdf = PBKDF2HMAC(algorithm=hashes.SHA256(), length=32, salt=salt, iterations=100000)
 chave_cofre = base64.urlsafe_b64encode(kdf.derive(password))
 cofre_cipher = Fernet(chave_cofre)
